1c8671ed5dcfbf630ec921099c1632dcda207708
Anton <aulyanov@ctrader.com>
2013-06-10 16:06:24 +0300

    removed testdriver

--- JsTestDriver/src/com/google/jstestdriver/javascript/browserloggerinit.js
+++
@@ -1,4 +0,0 @@
-
-jstestdriver.browserLogger = jstestdriver.BrowserLogger.create(window.top.location.toString(),
-    function() {jstestdriver.jQuery.ajax.apply(jstestdriver.jQuery,
-        arguments)});
